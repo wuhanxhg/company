a8d5a05aaf8aa2ca86d47df83a413738805b30e6
admin <admin@wuhanxhg.com>
2013-07-30 09:35:28 +0800

    add pages

---
+++ js/home.js
@@ -0,0 +1,18 @@
+//图片连续滚动代码
+var speed=20
+marDiv2.innerHTML=marDiv1.innerHTML
+marDiv3.innerHTML=marDiv1.innerHTML
+function Marquee(){
+	if(marDiv2.offsetWidth-marDiv.scrollLeft<=0)
+		marDiv.scrollLeft-=marDiv1.offsetWidth
+	else{
+		marDiv.scrollLeft++
+	}
+}
+var MyMar=setInterval(Marquee,speed)
+marDiv.onmouseover=function() {
+	clearInterval(MyMar)
+}
+marDiv.onmouseout=function() {
+	MyMar=setInterval(Marquee,speed)
+}
---
+++ js/index.js
@@ -0,0 +1,18 @@
+function marqueeShow(){
+	var speed=20;
+	demo2.innerHTML=demo1.innerHTML;
+	function Marquee(){ 
+		if(demo2.offsetTop-proPicShow.scrollTop<=0) 
+			proPicShow.scrollTop-=demo1.offsetHeight ;
+		else{ 
+			proPicShow.scrollTop++ ; 
+		} 
+	} 
+	var MyMar=setInterval(Marquee,speed);
+	proPicShow.onmouseover=function() {
+		clearInterval(MyMar);
+	} 
+	proPicShow.onmouseout=function() {
+		MyMar=setInterval(Marquee,speed);
+	} 		
+}
